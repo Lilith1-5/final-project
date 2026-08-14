46d2617561886aa76a58643ccd9808262df062b0
Lilith1-5 <Lilith222j@gmail.com>
2026-08-14 17:20:27 +1200

    Safety tips page (Fixed the nav bar but need some work)

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 function toggleNav() {
     document.getElementById("sidebar").classList.toggle("open");
 }
+
+const hamMenu = document.querySelector(".ham-menu");
+
+const offScreenMenu = document.querySelector(".off-screen-menu");
+
+hamMenu.addEventListener("click", () => {
+    hamMenu.classList.toggle("open");
+    offScreenMenu.classList.toggle("open");
+})
