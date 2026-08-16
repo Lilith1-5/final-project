7db145d64d616a105f528855df88ed900247d283
Lilith1-5 <Lilith222j@gmail.com>
2026-08-16 17:22:06 +1200

    Facts page(built the cards and swaping feature)

--- script.js
+++ script.js
@@ -10,3 +10,32 @@ hamMenu.addEventListener("click", () => {
     hamMenu.classList.toggle("open");
     offScreenMenu.classList.toggle("open");
 })
+
+let currentFact = 0;
+const cards = document.querySelectorAll(".fact-card");
+
+function updateFacts() {
+    cards.forEach((card, index) => {
+        card.classList.remove("active", "left", "right");
+
+        if (index === currentFact) {
+            card.classList.add("active");
+        } else if (index === currentFact - 1) {
+            card.classList.add("left");
+        } else if (index === currentFact + 1) {
+            card.classList.add("right");
+        }
+    });
+}
+
+function nextFact() {
+    currentFact = (currentFact + 1) % cards.length;
+    updateFacts();
+}
+
+function prevFact() {
+    currentFact = (currentFact - 1 + cards.length) % cards.length;
+    updateFacts();
+}
+
+updateFacts();
